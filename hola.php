f6501747fd2c0bbaf2b125418ac1d0ac70422cad
Pablo Lopez <pablorlopez8907@gmail.com>
2025-03-31 22:51:48 -0600

    codigo proyecto PHP

---
+++ hola.php
@@ -0,0 +1,6 @@
+
+<?php
+echo "Hola mundo, mi nombre es Pablo Lopez y estoy estudiando TDS en la Universidad Galileo 2025.";
+?>
+<img src="/actividad3/proyecto_php/imagen_lobo.webp" alt="imagenLobo">
+
